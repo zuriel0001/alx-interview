9beeea82d92d8ed6564a26f37ee55d9ad6debaab
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-01-26 00:01:27 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """
 Log parsing
+Script that reads stdin line by line and computes metrics
 """
 
 import sys
 
 if __name__ == '__main__':
 
-    filesize, count = 0, 0
+    filesize = 0
+    count = 0
     codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     stats = {k: 0 for k in codes}
 
@@ -21,19 +23,24 @@ if __name__ == '__main__':
         for line in sys.stdin:
             count += 1
             data = line.split()
+
             try:
                 status_code = data[-2]
                 if status_code in stats:
                     stats[status_code] += 1
             except BaseException:
                 pass
+
             try:
                 filesize += int(data[-1])
+
             except BaseException:
                 pass
+
             if count % 10 == 0:
                 print_stats(stats, filesize)
         print_stats(stats, filesize)
+
     except KeyboardInterrupt:
         print_stats(stats, filesize)
         raise
